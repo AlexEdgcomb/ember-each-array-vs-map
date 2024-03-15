fa031c0565b62d439b2a1c7c6d76b696acc6456b
AlexEdgcomb <aedgcomb@gmail.com>
2024-03-15 15:26:17 -0700

    Add person does NOT yield reset timers

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,13 +1,15 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
 import { tracked } from 'tracked-built-ins';
 
 const scientists = ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
 
 export default class PeopleListComponent extends Component {
-  people = tracked([]);
+  @tracked people = [];
 
   @action addPerson() {
     this.people.push(scientists.pop());
+
+    set(this, 'people', this.people);
   }
 }
